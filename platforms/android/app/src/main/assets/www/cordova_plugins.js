9b46c01c02e336a476485872d63a99068e48a675
Miqueias <miqueias.caetano@doctum.edu.br>
2020-09-16 15:04:14 -0300

    ok

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,106 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-clipboard.Clipboard",
+    "file": "plugins/cordova-clipboard/www/clipboard.js",
+    "pluginId": "cordova-clipboard",
+    "clobbers": [
+      "cordova.plugins.clipboard"
+    ]
+  },
+  {
+    "id": "cordova-promise-polyfill.Promise",
+    "file": "plugins/cordova-promise-polyfill/www/Promise.js",
+    "pluginId": "cordova-promise-polyfill",
+    "runs": true
+  },
+  {
+    "id": "cordova-promise-polyfill.promise.min",
+    "file": "plugins/cordova-promise-polyfill/www/promise.min.js",
+    "pluginId": "cordova-promise-polyfill"
+  },
+  {
+    "id": "cordova-plugin-admob-free.AdMob",
+    "file": "plugins/cordova-plugin-admob-free/www/admob.js",
+    "pluginId": "cordova-plugin-admob-free",
+    "clobbers": [
+      "admob",
+      "AdMob",
+      "plugins.AdMob"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification_android",
+    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-insomnia.Insomnia",
+    "file": "plugins/cordova-plugin-insomnia/www/Insomnia.js",
+    "pluginId": "cordova-plugin-insomnia",
+    "clobbers": [
+      "window.plugins.insomnia"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-admob-sdk": "0.24.1",
+  "cordova-clipboard": "1.3.0",
+  "cordova-promise-polyfill": "0.0.2",
+  "cordova-plugin-admob-free": "0.27.0",
+  "cordova-plugin-console": "1.0.7",
+  "cordova-plugin-dialogs": "1.2.1",
+  "cordova-plugin-insomnia": "4.3.0",
+  "cordova-plugin-splashscreen": "6.0.0",
+  "cordova-plugin-statusbar": "2.1.3",
+  "cordova-plugin-whitelist": "1.2.2",
+  "es6-promise-plugin": "4.2.2",
+  "cordova-plugin-x-socialsharing": "5.6.5",
+  "cordova-support-google-services": "1.4.1",
+  "phonegap-plugin-multidex": "1.0.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ www/js/firebase.js
@@ -0,0 +1,22 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBpg_xbTI83vuOjHJNF0WLrTmXukglCQ84",
+  authDomain: "gerador-de-documentos-86c72.firebaseapp.com",
+  databaseURL: "https://gerador-de-documentos-86c72.firebaseio.com",
+  projectId: "gerador-de-documentos-86c72",
+  storageBucket: "gerador-de-documentos-86c72.appspot.com",
+  messagingSenderId: "34113913862",
+  appId: "1:34113913862:web:6674353c2bc9f152ceaf87",
+  measurementId: "G-HSR76LH18G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+firebase.auth().signInAnonymously().catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorMessage)
+});
